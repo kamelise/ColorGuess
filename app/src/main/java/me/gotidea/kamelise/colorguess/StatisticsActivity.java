da1755660dcc4175187f67e30ed128c7525e4a85
kamelise <alena.shilova@gmail.com>
2018-03-21 00:26:30 +0200

    Added statistics activity

--- app/src/main/java/me/gotidea/kamelise/colorguess/StatisticsActivity.java
+++ app/src/main/java/me/gotidea/kamelise/colorguess/StatisticsActivity.java
@@ -1,29 +1,15 @@
 package me.gotidea.kamelise.colorguess;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class StatisticsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-//        mVisible = true;
-//        mControlsView = findViewById(R.id.fullscreen_content_controls);
-//        mContentView = findViewById(R.id.fullscreen_content);
-//
-//        // Set up the user interaction to manually show or hide the system UI.
-//        mContentView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                toggle();
-//            }
-//        });
+        setContentView(R.layout.activity_statistics);
     }
 
     @Override
@@ -42,21 +28,5 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility(uiOptions);
-    }
-
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.play_game:
-                Intent intent = new Intent(this, GameActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.stats:
-                startActivity(new Intent(this, StatisticsActivity.class));
-                break;
-            case R.id.settings:
-                break;
-            case R.id.help:
-                break;
-        }
     }
 }
